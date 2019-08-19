51d5b29374447694b1c65000aa754290bc31c8ec
zharabedian <zharabedian@gmail.com>
2019-08-18 21:46:25 -0400

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var friends = [
+    {
+        name: "Zak",
+        photo: "https://media.licdn.com/dms/image/C5603AQHPPaIU3xX21g/profile-displayphoto-shrink_200_200/0?e=1571875200&v=beta&t=GeA6PPYOwUZc3F10uj6KBiB6YQp3So9DVy5gPEXCcxU",
+    },
+    {
+        name: "Brittany",
+        photo: "https://d1hbpr09pwz0sk.cloudfront.net/profile_pic/brittany-waser-8d5fa826",
+    
+    },
+
+
+module.exports = friends;
